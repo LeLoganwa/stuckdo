ee6d57a57d2b2630cd46ac3b4e6b4fcff9f4fa60
SJha <skr.blr@gmail.com>
2015-06-07 22:37:44 +0530

    addedd tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606073659) do
+ActiveRecord::Schema.define(version: 20150607170148) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20150606073659) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
